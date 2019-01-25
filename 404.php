6f067e45f1475e2dd1dd08bdb6fe122122d5f961
BigCoke <45323514+BigCoke233@users.noreply.github.com>
2019-01-25 16:07:22 +0800

    Add files via upload

--- 404.php
+++ 404.php
@@ -3,7 +3,8 @@
 
   <div class="post-body">
     <div class="404">
-     <h1>天呐撸！又是404！</h1>
+     <h1>404</h1>
+	 <h3>一些东西总会在你不经意间溜走</h3>
 	 <a href="/"><i class="fa fa-home"></i> 点我回到首页</a>
 	</div>
  </div>
